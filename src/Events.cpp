523c3c478b1987dfc4359680f10e142c832fdcac
SiYuhan <489365227@qq.com>
2023-11-02 08:57:05 +0800

    move camera when right button pressed

--- src/Events.cpp
+++ src/Events.cpp
@@ -5,21 +5,25 @@ void MouseMoveCallback(GLFWwindow* window, double x, double y) {
 	static double last_x;
 	static double last_y;
 
-	if (first_call) {
-		last_x = x;
-		last_y = y;
-		first_call = false;
-		return;
-	}
+	auto state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT);
 
-	g_camera.ProcessMouseMove(x - last_x, y - last_y);
+	if (state == GLFW_PRESS) {
+		if (first_call) {
+			last_x = x;
+			last_y = y;
+			first_call = false;
+			return;
+		}
+
+		g_camera.ProcessMouseMove(x - last_x, y - last_y);
+	}
 }
 
 void ProcessKeyEvents(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
-	static float speed = 0.1f;
+	static float speed = 0.05f;
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
 		g_camera.Move(glm::vec3(0, 0, speed));
 	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
